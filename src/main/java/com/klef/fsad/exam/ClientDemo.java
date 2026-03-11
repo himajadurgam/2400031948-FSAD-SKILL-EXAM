c3bc8041423c695252693a86626f206bb44e8f7b
himajadurgam <himaja1083@gmail.com>
2026-03-11 10:58:28 +0530

    Add CRUD operations for Inventory using Hibernate
    Implemented Hibernate session management for CRUD operations on Inventory.

--- src/main/java/com/klef/fsad/exam/ClientDemo.java
+++ src/main/java/com/klef/fsad/exam/ClientDemo.java
@@ -1,5 +1,36 @@
 package com.klef.fsad.exam;
 
-public class ClientDemo {
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
+public class ClientDemo
+{
+ public static void main(String[] args)
+ {
+  SessionFactory sf = new Configuration().configure().buildSessionFactory();
+  Session s = sf.openSession();
+  Transaction t = s.beginTransaction();
+
+  // Insert
+  Inventory i = new Inventory();
+  i.name="Laptop";
+  i.description="Dell Laptop";
+  i.status="Available";
+  s.save(i);
+
+  // Delete by ID
+  Inventory obj = s.get(Inventory.class,1);
+  if(obj!=null)
+  {
+   s.delete(obj);
+  }
+
+  t.commit();
+  s.close();
+  sf.close();
+
+  System.out.println("Operations completed");
+ }
 }
